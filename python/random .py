4859c6bb647c610bfbc766e5cc6cd9b3b1c0b687
kenya6111 <kenyanke6111@gmail.com>
2024-08-22 20:00:41 +0900

    randintの引数修正

--- python/random .py
+++ python/random .py
@@ -14,15 +14,12 @@ def create_groups(group1, group2, group, num):
 if __name__ == '__main__':
     group=["A","B","C","D","E","F"]
 
-    division_type = random.randint(0,1)
+    division_type = random.randint(3,4)
 
     result_group1=[]
     result_group2=[]
 
-    if division_type == 0:
-        create_groups(result_group1, result_group2, group, 4)
-    if division_type == 1:
-        create_groups(result_group1, result_group2, group, 3)
+    create_groups(result_group1, result_group2, group, division_type)
 
-    print(result_group1)
     print(result_group2)
+    print(result_group1)
