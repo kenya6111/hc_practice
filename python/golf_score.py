1a9758e9551d2d9713c7893d1995a16e11006cc5
kenya6111 <kenyanke6111@gmail.com>
2024-08-28 20:10:06 +0900

    【ゴルフスコア判定】 リファクタリング

--- python/golf_score.py
+++ python/golf_score.py
@@ -1,40 +1,46 @@
+import sys
 
 def regulation_strokes_check(list_regulation_strokes):
     if len(list_regulation_strokes) != 18:
         print("入力する数値の個数は18個です。")
+        sys.exit(1)  # プログラムを終了する
     new_regulation_strokes_list = [ v for v in list_regulation_strokes if v >=3 and v <=5 ]
     if len(list_regulation_strokes) !=  len(new_regulation_strokes_list):
         print("入力できる値は3~5です。")
+        sys.exit(1)  # プログラムを終了する
 
-def input_actual_strokes_check(list_actual_strokes):
+def actual_strokes_check(list_actual_strokes):
     if len(list_actual_strokes) != 18:
         print("入力する数値の個数は18個です。")
+        sys.exit(1)  # プログラムを終了する
     new_actual_strokes_list = [ v for v in list_actual_strokes if v >= 1 ]
     if len(list_actual_strokes) !=  len(new_actual_strokes_list):
         print("入力できる値は1以上です。")
+        sys.exit(1)  # プログラムを終了する
 
 def score_judge(regulation_strokes, actual_strokes ,score_list):
     for v1,v2 in zip(regulation_strokes, actual_strokes):
-        if v1==v2:
+        diff = v1-v2
+
+        if diff == 0:
             score_list.append("パー")
-        elif v1-v2==1:
+        elif diff == 1:
             score_list.append("バーディ")
-        elif v1-v2==2:
-            if v1==3:
-                score_list.append("イーグル")
+        elif diff == 2:
+            if v1 == 3 or v2 == 1:
+                score_list.append("ホールインワン")
             else:
                 score_list.append("イーグル")
-        elif v1-v2==3:
+        elif diff == 3:
             if v1 == 4:
                 score_list.append("ホールインワン")
             else:
                 score_list.append("アルバトロス")
-        elif v1-v2==4:
-            if v1==5:
+        elif diff == 4 and v1 == 5:
                 score_list.append("コンドル")
-        elif v2==1:
+        elif v2 == 1:
             score_list.append("ホールインワン")
-        elif v1-v2 == -1:
+        elif diff == -1:
             score_list.append("ボギー")
         else:
             score_list.append(f"{v2-v1}ボギー")
@@ -44,7 +50,7 @@ if __name__ == "__main__":
     regulation_strokes = list(map(int, input().split(',')))
     actual_strokes = list(map(int, input().split(',')))
     regulation_strokes_check(regulation_strokes)
-    input_actual_strokes_check(actual_strokes)
+    actual_strokes_check(actual_strokes)
 
     score_list=[]
     score_judge(regulation_strokes, actual_strokes ,score_list)
